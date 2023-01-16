efcb3232e8261d3fdd00d5686d570d70f02819c9
= <=>
2023-01-16 06:27:31 +0100

    update backend

--- backend/test.php
+++ backend/test.php
@@ -14,10 +14,12 @@
 
     // Check connection
     if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
     }
 
-    if (isset($_GET['meals'])){
+    function getMeals(){
+        global $conn;
+        
         $meal_id = $_GET['meals'];
 
         if($meal_id == -1){
@@ -166,10 +168,139 @@
 
             echo json_encode($new_item);
         }
-        
-        
     }
 
+    function getEvents(){
+
+        global $conn;
+        $event_id = $_GET["events"];
+
+        if( $event_id == -1 ){
+            // $sql = "select t1.*, t2.* from 
+            // (select ticket_id as id, event_id, ticket_description as description, ticket_price as fee, ticket_members as attendees from wp_em_tickets) as t1
+            // left join
+            // (select event_id as eid,post_id,event_owner, event_status, event_name as name, event_start_date as date, event_start_time as time ,event_all_day, post_content,location_id  from wp_em_events) as t2
+            // on t1.event_id = t2.eid
+            // order by t1.id desc
+            // ";
+
+            $sql = "select t5.*, t6.comment_content as description from
+            (select t3.*, t4.display_name as name from 
+            (select t1.*,t2.* from
+            (select event_id as id, event_name as title, post_id, post_content, event_owner, event_status, event_start_date as date, event_start_time as time from wp_em_events) as t1
+            left join
+            (select event_id, count(booking_id) as peopleAttending, sum(booking_spaces) as ticket from wp_em_bookings group by event_id) as t2
+            on t1.id = t2.event_id) as t3
+            left join
+            wp_users t4
+            on t3.event_owner = t4.ID) as t5
+            left join
+            wp_comments t6
+            on t5.post_id = t6.comment_post_ID";
+
+            $result = $conn->query($sql);
+
+            $response_data = array();
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc() ) {
+                    $new_item["id"] = $row["id"];
+                    $new_item["title"] = $row["title"];
+                    $new_item["liveNow"] = $row["event_status"]==1?true:false;
+                    $new_item["peopleAttending"] = $row["peopleAttending"]!=''?$row["peopleAttending"]:0;
+                    $new_item["ticket"] = $row["ticket"]!=''?$row["ticket"]:0;
+                    $new_item["date"] = $row["date"];
+                    $new_item["time"] = $row["time"];
+                    $new_item["name"] = $row["name"];
+                    $new_item["description"] = preg_replace('#(\[(.*)\](.*)\[/.*\])#Us','',$row["post_content"]);
+
+                    //adding picture
+                    preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $row["post_content"], $match);
+                    
+                    foreach($match[0] as $url){
+                        if( strpos($url, "upload") )
+                        {
+                            $new_item["images"] = $url;
+                            break;
+                        }    
+                    }                   
+                    
+                    array_push($response_data, $new_item);
+                }
+            }
+            echo json_encode($response_data);
+        }else{
+
+            $sql = "select t5.*, t6.comment_content as description from
+            (select t3.*, t4.display_name as eventManager from 
+            (select t1.*,t2.* from
+            (select event_id as id, event_name as name, post_id, post_content, event_owner, event_status, event_start_date as date, event_start_time as time from wp_em_events where event_id = ".$event_id.") as t1
+            left join
+            (select event_id, count(booking_id) as peopleAttending, sum(booking_spaces) as ticket from wp_em_bookings group by event_id) as t2
+            on t1.id = t2.event_id) as t3
+            left join
+            wp_users t4
+            on t3.event_owner = t4.ID) as t5
+            left join
+            wp_comments t6
+            on t5.post_id = t6.comment_post_ID
+            ";
+
+            $result = $conn->query($sql);
+
+            $new_item = array();
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc() ) {
+                    $new_item["id"] = $row["id"];
+                    $new_item["liveNow"] = $row["event_status"]==1?true:false;
+                    $new_item["peopleAttending"] = $row["peopleAttending"]!=''?$row["peopleAttending"]:0;
+                    $new_item["ticket"] = $row["ticket"]!=''?$row["ticket"]:0;
+                    $new_item["date"] = $row["date"];
+                    $new_item["time"] = $row["time"];
+                    $new_item["name"] = $row["name"];
+                    $new_item["eventManager"] = $row["eventManager"];
+                    $new_item["host"] = $row["eventManager"];
+                    
+                    $new_item["liveStreamUrl"] = "https://goosebumps.finance/images/logo-icon.png";
+                    $new_item["gate"] = 6;
+                    $new_item["seating"] = "free";
+                    $new_item["accessRestrictions"] = "Staff, VIPs, Executive Members, All Executives of DDCMS";
+                    
+                    $new_item["presenters"] = array();
+                    $presenter1["name"] = $row["eventManager"];
+                    $presenter1["starPresenter"] = true;
+                    $presenter1["createdBy"] = "Dept for Digital Culture, Media & Sport";
+                    array_push($new_item["presenters"], $presenter1);
+
+                    $new_item["description"] = preg_replace('#(\[(.*)\](.*)\[/.*\])#Us','',$row["post_content"]);
+
+                    //adding picture
+                    preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $row["post_content"], $match);
+                    
+                    $new_item["images"] = array();
+
+                    foreach($match[0] as $url){
+                        if( strpos($url, "upload") )
+                            array_push($new_item["images"], $url);
+                    }                   
+                    
+                }
+            }
+            echo json_encode($new_item);
+
+        }
+    }
+
+    
+    if (isset($_GET['meals'])){
+        getMeals();
+    }
+    else if( isset($_GET['events'])){
+        getEvents();
+    }
     $conn->close();
 
 ?>
