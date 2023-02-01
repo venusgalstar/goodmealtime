b4a62acf94414b922e3c14ee3430221b483e263e
CryptoSnowPrince <topdirector2017@gmail.com>
2023-02-02 01:26:33 +0900

    add react-native-geolocation-service add context

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,25 @@
 import { StyleSheet, View } from 'react-native'
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import { RootNavigation } from '../src/navigations'
 
+export const AppStateContext = createContext({});
+
 const App = () => {
+  const setGeoData = (_geoData: any) => {
+    setGeo(_geoData)
+  }
+
+  const [geo, setGeo] = useState({
+    geoData: { latitude: 0, longitude: 0 },
+    setGeoData
+  })
+
   return (
-    <View style={Styles.container}>
-      <RootNavigation />
-    </View>
+    <AppStateContext.Provider value={geo}>
+      <View style={Styles.container}>
+        <RootNavigation />
+      </View>
+    </AppStateContext.Provider>
   )
 }
 
--- app/src/screens/home/Home.tsx
+++ app/src/screens/home/Home.tsx
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Container from '../../components/Container'
 import { Colors, Images } from '../../res'
 import EventsMealsBar from './EventsMealsBar'
@@ -8,6 +8,7 @@ import MyTableBtn from './MyTableBtn'
 import TableSlider from './TableSlider'
 import BottomBar from './BottomBar'
 import { Cards } from '../cards';
+import { AppStateContext } from '../../App'
 // import { API_PATH, REFETCH } from '../../config'
 // import { mealsData, eventsData } from './Data'
 // import { getAsyncData, postAsyncData } from '../../utils/fetch'
@@ -29,6 +30,11 @@ const Home = (props: any) => {
             id: '3'
         }
     ])
+    const x = useContext(AppStateContext)
+
+    useEffect(() => {
+        console.log(x)
+    }, [])
 
 
     const onTableBtnPress = () => {
